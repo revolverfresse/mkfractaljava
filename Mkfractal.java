6f66cb3f10eb712536c6db3c679a9c0e08678490
revolverfresse <thorstenkiefer32@gmail.com>
2025-01-04 11:58:28 +0100

    Add files via upload

---
+++ Mkfractal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mkfractal;
+
+/**
+ *
+ * @author thorsten
+ */
+public class Mkfractal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new NewJFrame().setVisible(true);
+    }
+}
